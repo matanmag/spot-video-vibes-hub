e6d8dd62f101fd09351b810d6c93a088a67dd0b8
matan <matanmag@gmail.com>
2025-07-10 14:19:06 +0300

    WIP: local changes before merge

--- src/components/AppBottomNav.tsx
+++ src/components/AppBottomNav.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Home, TrendingUp, Map, User, Plus } from 'lucide-react';
+import SearchButton from './SearchButton';
 
 const NavIcon: React.FC<{ icon: React.ElementType; path: string }> = ({ icon: Icon, path }) => {
   const location = useLocation();
@@ -23,7 +23,8 @@ const AppBottomNav = () => {
     return (
         <nav className="fixed bottom-0 left-0 right-0 flex justify-around items-center bg-black/30 py-3 backdrop-blur-md border-t border-white/10 z-50">
             <NavIcon icon={Home} path="/home" />
-            <NavIcon icon={TrendingUp} path="/search" />
+            <NavIcon icon={TrendingUp} path="/trending" />
+            <SearchButton />
             <Link to="/upload" className="bg-[#00e0ff] p-3 rounded-xl shadow-lg active:scale-95 transition-transform">
                 <Plus className="w-6 h-6 text-[#071b2d]" />
             </Link>
